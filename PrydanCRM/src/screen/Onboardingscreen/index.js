3b73c3eee00a7d8810c5614e049e4d08256f5f9d
naisargi-prydan <naisargi.prydan@gmail.com>
2022-01-13 10:55:29 +0530

    add image path

--- PrydanCRM/src/screen/Onboardingscreen/index.js
+++ PrydanCRM/src/screen/Onboardingscreen/index.js
@@ -13,7 +13,7 @@ const Onboarding = ({navigation}) =>{
   return (
       <Swiper style={styles.wrapper} showsButtons={false}>
         <View style={styles.slide1}>
-        <Image source={require('D:/Naisargi Trivedi/Delivery/CRM-API/PrydanCRM/src/assets/Images/logo.png')}/>
+        <Image source={require('D:/Naisargi Trivedi/Delivery/ReactNative/PrydanCRM/src/assets/Images/logo.png')}/>
        {/* <Spacer spaceMargin={20}/> */}
           <Text style={styles.title}>Prydan Growth Stock</Text>
           <Text style={styles.description}> Powerful alone.Even together. Businesses grow faster when they have a complete Growth Stack for marketing, sales, and CRM from prydan.</Text>
@@ -33,7 +33,7 @@ const Onboarding = ({navigation}) =>{
           </View>
 
           <View style={styles.slide2}>
-          <Image source={require('D:/Naisargi Trivedi/Delivery/CRM-API/PrydanCRM/src/assets/Images/logo.png')}/>
+          <Image source={require('D:/Naisargi Trivedi/Delivery/ReactNative/PrydanCRM/src/assets/Images//logo.png')}/>
            <Text style={styles.title}>Prydan CRM</Text>
            <Text style={styles.description}>Automate data entry and manual tasks,gain   visibility into your pip line, and keep contacts organized so you can close more deals with less work.</Text>
            <Text style={styles.subtitle}>Free version of every prydan product. start now upgrade as you grow</Text>
@@ -52,7 +52,7 @@ const Onboarding = ({navigation}) =>{
         </View>
 
         <View style={styles.slide3}>
-        <Image source={require('D:/Naisargi Trivedi/Delivery/CRM-API/PrydanCRM/src/assets/Images/logo.png')}/>
+        <Image source={require('D:/Naisargi Trivedi/Delivery/ReactNative/PrydanCRM/src/assets/Images/logo.png')}/>
           <Text style={styles.title}>Marketing Free</Text>
           <Text style={styles.description}>Learn more about the people visiting site and what pages convert the most leads with easy-to-build lead forms and built-in analytics.</Text>
            <Text style={styles.subtitle}>Free version of every prydan product. start now upgrade as you grow</Text>
@@ -71,7 +71,7 @@ const Onboarding = ({navigation}) =>{
         </View>
 
         <View style={styles.slide3}>
-        <Image source={require('D:/Naisargi Trivedi/Delivery/CRM-API/PrydanCRM/src/assets/Images/logo.png')}/>
+        <Image source={require('D:/Naisargi Trivedi/Delivery/ReactNative/PrydanCRM/src/assets/Images/logo.png')}/>
           <Text style={styles.title}>Sales Free</Text><Text style={styles.description}>Track email opens and clicks, spend less time writing emails, log calls automotically, and speed up your sales process..</Text>
            <Text style={styles.subtitle}>Free version of every prydan product. start now upgrade as you grow</Text>
            <View style={{flexDirection:'row'}}>
--- PrydanCRM/src/screen/index.js
+++ PrydanCRM/src/screen/index.js
@@ -1,4 +1,4 @@
 /* eslint-disable prettier/prettier */
-export {default as Login} from './Login/Index';
-export {default as Signup} from './Signup/Index';
-export {default as Onboardingscreen} from './Onboardingscreen/Index';
+export {default as Login} from './Login/index';
+export {default as Signup} from './Signup/index';
+export {default as Onboardingscreen} from './Onboardingscreen/index';
